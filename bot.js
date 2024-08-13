e7a0c891fb37aca5dfa7f678967948fc86380732
Prayush Adhikari <115662438+adhikareeprayush@users.noreply.github.com>
2024-08-13 09:45:27 +0545

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,8 @@ const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 
 // Replace with your bot's token and TMDb API key
-const token = 'YOUR_BOT"S_TOKEN';
-const tmdbApiKey = 'YOUR_TMDb_API_KEY';
+const token = 'YOUR_BOTS_TOKEN';
+const tmdbApiKey = 'YOUR_TMDb_API_TOKEN';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
